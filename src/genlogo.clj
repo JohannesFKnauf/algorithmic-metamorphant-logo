d6cf4f6765b323de0e27143dfae9e7e0bde2eb63
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2020-01-12 22:38:29 +0100

    refactor logo to function taking input parameters as map

--- src/genlogo.clj
+++ src/genlogo.clj
@@ -4,12 +4,8 @@
 (def golden-ratio
   1.61803398875)
 
-(def logo
+(defn logo [{:keys [dot-radius line-width corner-arc-radius eye-offset]}]
   (let [canvas {:width 1000 :height 1000}
-        dot-radius 55
-        line-width 64
-        corner-arc-radius 160
-        eye-offset 265
         eye {:x (- (:width canvas) eye-offset)
              :y eye-offset}
         inner-outline-dot {:x (/ (:width canvas) (+ 1 golden-ratio))
@@ -28,10 +24,10 @@
         view-box-padding-y view-box-padding-x
         total-width (- (:width canvas) (* 2 view-box-padding-x))
         total-height (- (:height canvas) (* 2 view-box-padding-y))
-        line-style {:stroke "black" :path-length 1000 :stroke-width line-width :stroke-linejoin "round" :fill "none" :marker-start "url(#bm)" :marker-end "url(#bm)"}]
+        line-style {:stroke "#02324b" :path-length 1000 :stroke-width line-width :stroke-linejoin "round" :fill "none" :marker-start "url(#bm)" :marker-end "url(#bm)"}]
       [:dali/page {:width "100%" :height "100%" :view-box (str view-box-padding-x " " view-box-padding-y " " total-width " "total-height)}
        [:defs
-        [:circle {:id "bubbel" :cx 0 :cy 0 :r dot-radius :fill "black"}]
+        [:circle {:id "bubbel" :cx 0 :cy 0 :r dot-radius :fill "#02324b"}]
         [:marker {:id "bm" :view-box (str "0 0 " dot-diameter " " dot-diameter) :ref-x dot-radius :ref-y dot-radius :marker-units "userSpaceOnUse" :marker-width dot-diameter :marker-height dot-diameter}
          [:use {:x dot-radius :y dot-radius :xlink:href "#bubbel"}]]]
        [:path line-style
@@ -87,16 +83,20 @@
         [(- (:x ear-end) corner-arc-radius)
          (- (:height canvas) line-padding)]]
        [:use {:x (:x eye) :y (:y eye) :xlink:href "#bubbel"}]
-       [:rect {:x 0 :y 0 :width (:width canvas) :height (:height canvas) :fill "none" :stroke "blue" :stroke-width "4"}]  ; frame
-       [:line {:x1 (:x inner-outline-dot) :y1 0 :x2 (:x inner-outline-dot) :y2 (:height canvas) :stroke "blue" :stroke-width "4"}]  ; vertical: left inner dot positions
-       [:line {:x1 (:width canvas) :y1 0 :x2 0 :y2 (:height canvas) :stroke "blue" :stroke-width "4"}]   ; diagonal: eye positions
-       [:line {:x1 0 :y1 (:y eye) :x2 (:width canvas) :y2 (:y eye) :stroke "blue" :stroke-width "4"}]    ; horizontal: eye positions
-       [:line {:x1 0 :y1 (:y inner-outline-dot) :x2 (:width canvas) :y2 (:y inner-outline-dot) :stroke "blue" :stroke-width "4"}]    ; horizontal: inner outline dot positions
-       [:line {:x1 0 :y1 (:y inner-ear-dot) :x2 (:width canvas) :y2 (:y inner-ear-dot) :stroke "blue" :stroke-width "4"}]    ; horizontal: inner ear dot positions
-       [:line {:x1 (:x bottom-right-dot) :y1 0 :x2 (:x bottom-right-dot) :y2 (:height canvas) :stroke "blue" :stroke-width "4"}]  ; vertical: right dot position
-       [:line {:x1 (:x bottom-middle-dot) :y1 0 :x2 (:x bottom-middle-dot) :y2 (:height canvas) :stroke "blue" :stroke-width "4"}]  ; vertical: middle dot position
-       [:line {:x1 0 :y1 (:y ear-end) :x2 (:width canvas) :y2 (:y ear-end) :stroke "blue" :stroke-width "4"}]  ; horizontal: ear end line positions
+       ;; [:rect {:x 0 :y 0 :width (:width canvas) :height (:height canvas) :fill "none" :stroke "blue" :stroke-width "4"}]  ; frame
+       ;; [:line {:x1 (:x inner-outline-dot) :y1 0 :x2 (:x inner-outline-dot) :y2 (:height canvas) :stroke "blue" :stroke-width "4"}]  ; vertical: left inner dot positions
+       ;; [:line {:x1 (:width canvas) :y1 0 :x2 0 :y2 (:height canvas) :stroke "blue" :stroke-width "4"}]   ; diagonal: eye positions
+       ;; [:line {:x1 0 :y1 (:y eye) :x2 (:width canvas) :y2 (:y eye) :stroke "blue" :stroke-width "4"}]    ; horizontal: eye positions
+       ;; [:line {:x1 0 :y1 (:y inner-outline-dot) :x2 (:width canvas) :y2 (:y inner-outline-dot) :stroke "blue" :stroke-width "4"}]    ; horizontal: inner outline dot positions
+       ;; [:line {:x1 0 :y1 (:y inner-ear-dot) :x2 (:width canvas) :y2 (:y inner-ear-dot) :stroke "blue" :stroke-width "4"}]    ; horizontal: inner ear dot positions
+       ;; [:line {:x1 (:x bottom-right-dot) :y1 0 :x2 (:x bottom-right-dot) :y2 (:height canvas) :stroke "blue" :stroke-width "4"}]  ; vertical: right dot position
+       ;; [:line {:x1 (:x bottom-middle-dot) :y1 0 :x2 (:x bottom-middle-dot) :y2 (:height canvas) :stroke "blue" :stroke-width "4"}]  ; vertical: middle dot position
+       ;; [:line {:x1 0 :y1 (:y ear-end) :x2 (:width canvas) :y2 (:y ear-end) :stroke "blue" :stroke-width "4"}]  ; horizontal: ear end line positions
        ]))
 
 (defn -main []
-  (dali.io/render-svg logo "metamorphant.svg"))
+  (dali.io/render-svg (logo {:dot-radius 55
+                             :line-width 64
+                             :corner-arc-radius 160
+                             :eye-offset 265})
+                      "metamorphant.svg"))
